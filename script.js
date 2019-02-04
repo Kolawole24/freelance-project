e1d2ec1bd8562c525d7f0ab06ad1e814d0a42abe
Oyinda David <oyetunmibidavid9@gmail.com>
2019-02-04 05:16:53 +0000

    search feature

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 new Vue({
-  el: 'bidModal',
-  data(){
-    return{
-        clientPay: '',
-        frelancerPay: '',
+  el: '#app',
+  data() {
+    return {
+      selected: ''
     }
   },
   methods: {
-      lancerPay(){
-          
+    searchBySkill() {
+      let searchValue = document.getElementById('searchbox');
+      if (searchValue.value === '1') {
+        window.location.href = "webdev.html";
+      } else if (searchValue.value === '2') {
+        window.location.href = "webdes.html";
+      } else if (searchValue.value === '3') {
+        window.location.href = "grades.html";
+      } else if (searchValue.value === '4') {
+        window.location.href = "sysad.html";
+      } else if (searchValue.value === '5') {
+        window.location.href = "photo.html";
+      } else if (searchValue.value === '6') {
+        window.location.href = "digima.html";
       }
+    }
   }
 })
